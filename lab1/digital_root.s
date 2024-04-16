f7d2d4a636002075f9cfb79f2abb9d2c2f034dbf
Georgios Selivanof <gselivanof@gmail.com>
2024-04-16 18:35:23 +0300

    Less branches and formatting
    Improved performance by uses less branches
    Fixed some comments
    Improved formatting

--- lab1/digital_root.s
+++ lab1/digital_root.s
@@ -14,32 +14,29 @@
 
 digital_root:
 	.fnstart
-	
 	push {lr}
-	// Using registers:
-	// r0: Input Argument, Return Value
-	// r1: Reciprocal
-	// r2: Quotient of division by 9
-	ldr r1,  reciprocal     // Loading the reciprocal into r1
-	mov r2, #0              // Initialize r2 (result) to 0
+	                        // Using registers:
+	                        // r0: Input Argument, Return Value
+	                        // r1: Reciprocal
+	                        // r2: Quotient of division by 9
 	
-	cmp r0, #0             
-	beq end                 // If the input is 0, return 0
-		
-	bgt div9
-	rsb r0, r0, #0          // Convert negative number to positive
-div9:
-	umull r3, r2, r1, r0    // Multiply with the reciprocal
-	lsr r2, r2, #1          // Left shift on the lower 32 bits, the result requals r0 / 9
+	ldr r1,  reciprocal     // Load the reciprocal into r1
+	mov r2, #0              // Initialize the quotient (r2) to 0
 	
-	                        // Find the Remainder
-	add r2, r2, r2, lsl #3  // Multiply the quotient by 9 (Alternative intruction mul r2, r2, #9)
-	sub r0, r0, r2          // Remove the quotient * 9 from the original number, Get the remainder
+	cmp r0, #0            	// === Special cases ===
+	beq end                 // If the input (r0) is 0, return 0
+	it lt					// If the input (r0) is negative
+	rsblt r0, r0, #0        // Convert negative input (r0) to positive
+	                        // === Division By 9 ===
+	umull r3, r2, r1, r0    // Multiply the input (r0) with the reciprocal (r1)
+	lsr r2, r2, #1          // Right shift on the lower 32 bits of the result (r2). r2 is now the quotient of r0 / 9
+	                        // === Find the Remainder ===
+	add r2, r2, r2, lsl #3  // Multiply the quotient (r2) by 9
+	sub r0, r0, r2          // Remove the quotient * 9 (r2) from the input (r0). r0 is now the remainder of r0 / 9
 	
-	cmp r0, #0              // If the remainder is 0, the digital root is 9
-	bne end
-	mov r0, #9              // Change the result to 9
-	
+	cmp.N r0, #0            
+	it eq                   // If the remainder (r0) is 0
+	moveq r0, #9            // Change the result (r0) to 9
 end:
 	pop {pc}
 	.fnend
